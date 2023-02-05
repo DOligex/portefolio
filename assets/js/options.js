ea29248ea16a4c60faf30280e6361b36831957a6
Olivier Darbonville <odarbonville@explorance.com>
2023-02-05 18:49:04 -0500

    Update contact details

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,31 +1,30 @@
 // select html
-let copy = document.querySelector('#copyright');
+let copy = document.querySelector("#copyright");
 // variables
 let dateCopyright = new Date().getFullYear();
-let copyright = `${dateCopyright} © DOligex V1.2.1`;
+let copyright = `${dateCopyright} © DOligex V1.2.2`;
 // input
 copy.textContent = copyright;
-
 
 // dark-mode don't work getElementsById is not a fnction
 
 // let app = {
-    
+
 //     init: function () {
 //         app.changeMode();
 //     },
-    
+
 //     changeMode: function() {
 //         const btn = document.getElementsById('btn');
 //         btn.addEventListener('change', () => {
 //             app.darkMode();
 //         });
 //     },
-    
+
 //     darkMode: function() {
 //         const logo = document.getElementById('logo');
 //         const body = document.getElementById('body');
-        
+
 //         body.classList.toggle('dark');
 //         logo.classList.toggle('dark');
 //     }
